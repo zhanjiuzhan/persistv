cecbd90cbfa2c52a59dc20f2d3f54eb4df4da8ca
Raiden <18292963987@163.com>
2020-11-16 17:59:35 +0800

    feat: 1. 主页面代码提交

---
+++ persistv/src/layout/components/TagsView.vue
@@ -0,0 +1,205 @@
+<template>
+  <div class="tags-view-container">
+    <scroll-pane ref="scrollPane" class="tags-view-wrapper">
+      <router-link
+        v-for="tag in visitedViews"
+        ref="tag"
+        :class="isActive(tag)?'active':''"
+        :to="{ path: tag.path, qurey: tag.qurey, fullPath: tag.fullPath }"
+        :key="tag.path"
+        tag="span"
+        class="tags-view-item"
+        @click.middle.native="closeSelctedTag(tag)"
+        @contextmenu.prevent.native="openMenu(tag, $event)">
+        {{ tag.title }}
+        <span v-if="!tag.meta.affix" class="el-icon-close" @click.prevent.stop="closeSelectedTag(tag)" />
+      </router-link>
+    </scroll-pane>
+    <ul v-show="visible" :style="{ left:`${left}px`,top:`${top}px`}" class="contextMenu">
+      <li @click="refreshSelectedTag(selectedTag)">{{ refreshText }}</li>
+      <li v-if="!(selectedTag.meta&&selectedTag.meta.affix)" @click="closeSelectedTag(selectedTag)">{{ closeText }}</li>
+      <li @click="closeOthersTags">{{ closeOtherText }}</li>
+      <li @click="closeAllTags(selectedTag)">{{ closeAllText }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ScrollPane from '@/components/ScrollPane'
+import path from 'path'
+import { routerMap } from '@/router/routers'
+
+export default {
+  name: 'TagsView',
+
+  components: { ScrollPane },
+
+  data() {
+    return {
+      visible: false,
+      top: 0,
+      left: 0,
+      affixTags: [],
+      selectedTag: {},
+      refreshText: '刷新',
+      closeText: '关闭',
+      closeOtherText: '关闭其他',
+      closeAllText: '关闭所有'
+    }
+  },
+
+  computed: {
+    visitedViews () {
+      return this.$store.state.tagsView.visitedViews
+    }
+  },
+
+  watch: {
+    $route () {
+      this.addViewTags()
+      this.moveToCurrentTag()
+    },
+    visible(value) {
+      if (value) {
+        document.body.addEventListener('click', this.closeMenu)
+      } else {
+        document.body.removeEventListener('click', this.closeMenu)
+      }
+    }
+  },
+
+  mounted () {
+    this.initTags()
+    this.addViewTags()
+  },
+
+  methods: {
+    isActive (route) {
+      return route.path === this.$route.path
+    },
+    filterAffixTags(routes, basePath = '/') {
+      let tags = []
+      routes.forEach(route => {
+        if (route.meta && route.meta.affix) {
+          const tagPath = path.resolve(basePath, route.path)
+          tags.push({
+            fullPath: tagPath,
+            path: tagPath,
+            name: route.name,
+            meta: { ...route.meta }
+          })
+        }
+        if (route.children) {
+          const tempTags = this.filterAffixTags(route.children, route.path)
+          if (tempTags.length >= 1) {
+            tags = [...tags, ...tempTags]
+          }
+        }
+      })
+      return tags
+    },
+    initTags() {
+      const affixTags = this.affixTags = this.filterAffixTags(routerMap)
+      for (const tag of affixTags) {
+        if (tag.name) {
+          this.$route.dispatch('addVisitedView', tag)
+        }
+      }
+    },
+    addViewTags() {
+      const { name } = this.$route
+      if (name) {
+        this.$store.dipatch('addView', this.$route)
+      }
+      return false
+    },
+    moveToCurrentTag() {
+      const tags = this.$refs.tag
+      this.$nextTick(() => {
+        for (const tag of tags) {
+          if (tag.to.path === this.$route.path) {
+            this.$refs.scroollPane.moveToTarget(tag)
+
+            if (tag.to.fullPath !== this.$route.fullPath) {
+              this.$store.dipatch('updateVisitedView', this.$route)
+            }
+            break
+          }
+        }
+      })
+    },
+    refreshSelectedTag(view) {
+      this.$store.dispatch('delCacheView', view).then(() => {
+        const { fullPath } = view
+        this.$nextTick(() => {
+          this.$router.replace({
+            path: '/redirect' + fullPath
+          })
+        })
+      })
+    },
+    closeSelectedTag(view) {
+      this.$store.dispatch('delView', view).then(({ visitedViews }) => {
+        if (this.isActive(view)) {
+          const latestView = visitedViews.slice(-1)[0]
+          if (latestView) {
+            this.$router.push(latestView)
+          } else {
+            this.$router.push('/')
+          }
+        }
+      })
+    },
+    closeOthersTags() {
+      this.$router.push(this.selectedTag)
+      this.$store.dispatch('delOthersViews', this.selectedTag).then(() => {
+        this.moveToCurrentTag()
+      })
+    },
+    closeAllTags(view) {
+      this.$store.dispatch('delAllViews').then(({ visitedViews }) => {
+        if (this.affixTags.some(tag => tag.path === view.path)) {
+          return
+        }
+        this.toLastView(visitedViews, view)
+      })
+    },
+    toLastView(visitedViews, view) {
+      const latestView = visitedViews.slice(-1)[0]
+      if (latestView) {
+        this.$router.push(latestView)
+      } else {
+        if (view.name === '首页') {
+          this.$router.replace({ path: '/redirect' + view.fullPath })
+        } else {
+          this.$router.push('/')
+        }
+      }
+    },
+    openMenu(tag, e) {
+      const menuMinWidth = 105
+      const offsetLeft = this.$el.getBoundingClientRect().left
+      const offsetWidth = this.$el.offsetWidth
+      const maxLeft = offsetWidth - menuMinWidth
+      const left = e.clientX - offsetLeft + 15
+
+      if (left > maxLeft) {
+        this.left = maxLeft
+      } else {
+        this.left = left
+      }
+      this.top = e.clientY
+
+      this.visible = true
+      this.selectedTag = tag
+    },
+    closeMenu() {
+      this.visibale = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
